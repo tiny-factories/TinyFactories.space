b1697741bb728e2073dd75d43e157f2b9c1883d8
Glitch (tiny-factories) <none>
2020-01-23 19:59:35 +0000

    💿🎓 Updated with Glitch

--- index.js
+++ index.js
@@ -1,278 +1,239 @@
-// server.js
-// where your node app starts
+var nameArray = ["writing", "making", "socializing", "communicating", "reading", "playing", "exploring", "learning"]; //One of those will be randomly chosen
 
-// init project
-const express = require("express");
-const app = express();
+var lastIndex = 0; //Used to remember the last word picked
 
-// we've started you off with Express,
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+//This function will get a random INT used later as the array index
+function getRandomInt(iMin, iMax) {
+    var newIndex = Math.floor(Math.random() * (iMax - iMin)) + iMin;
+    while( lastIndex == newIndex ){
+        var newIndex = Math.floor(Math.random() * (iMax - iMin)) + iMin;
+    }
+  lastIndex = newIndex;  
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
+  return newIndex;
+}
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function(request, response) {
-  response.sendFile(__dirname + "/views/index.html");
-});
-app.get("/faq.html", function(request, response) {
-  response.sendFile(__dirname + "/views/faq.html");
-});
-app.get("/projects.html", function(request, response) {
-  response.sendFile(__dirname + "/views/projects.html");
-});
-app.get("/blog.html", function(request, response) {
-  response.sendFile(__dirname + "/views/blog.html");
-});
-app.get("/opencall.html", function(request, response) {
-  response.sendFile(__dirname + "/views/opencall.html");
-});
-app.get("/live.html", function(request, response) {
-  response.sendFile(__dirname + "/views/live.html");
-});
+function pickRandomName() {
+    var domHeaderName = document.getElementById("nameChange"); //Put the ID of the DOM element you want to change here
+    var randomIndex = getRandomInt(0, nameArray.length);
 
-// listen for requests :)
-const listener = app.listen(process.env.PORT, function() {
-  console.log("Your app is listening on port " + listener.address().port);
-});
+    //Put the randomly chosen name in the DOM
+    domHeaderName.innerHTML = nameArray[randomIndex];
+    //loop
+    setTimeout(pickRandomName, 2000); //Loop every 2000ms (2s)
+}
+
+pickRandomName();
+
+///
+var date = new Date().getFullYear();
+
+document.getElementById("year").innerHTML = date;
+
+///
 
 
-// var nameArray = ["writing", "making", "socializing", "communicating", "reading", "playing", "exploring", "learning"]; //One of those will be randomly chosen
+///
+/*
+ Inspired by https://soulwire.co.uk/
+*/
 
-// var lastIndex = 0; //Used to remember the last word picked
+// TODO: revert back to source text after animation is complete
+// Don't wrap the <span> around the source text, clone first -> then wrap
 
-// //This function will get a random INT used later as the array index
-// function getRandomInt(iMin, iMax) {
-//     var newIndex = Math.floor(Math.random() * (iMax - iMin)) + iMin;
-//     while( lastIndex == newIndex ){
-//         var newIndex = Math.floor(Math.random() * (iMax - iMin)) + iMin;
-//     }
-//   lastIndex = newIndex;  
+const hypertext = (function() {
+    function countLines(elem) {
+        let lineHeight = window.getComputedStyle(elem, null).getPropertyValue('line-height');
 
-//   return newIndex;
-// }
+        //     Box Height          Line Height
+        return elem.offsetHeight / parseInt(lineHeight);
+    }
 
-// function pickRandomName() {
-//     var domHeaderName = document.getElementById("nameChange"); //Put the ID of the DOM element you want to change here
-//     var randomIndex = getRandomInt(0, nameArray.length);
+    function getLines(el) {
+        let lineCount = countLines(el);
+        let html = el.innerHTML.replace(/(\S+\s*)/g, '<span>$1</span>');
 
-//     //Put the randomly chosen name in the DOM
-//     domHeaderName.innerHTML = nameArray[randomIndex];
-//     //loop
-//     setTimeout(pickRandomName, 2000); //Loop every 2000ms (2s)
-// }
+        el.innerHTML = html;
 
-// pickRandomName();
+        var offset = 0;
+        var spans = document.querySelectorAll('.hypertext span');
 
-// ///
-// var date = new Date().getFullYear();
+        function getLine(index) {
+            var top = 0,
+                buffer = [];
 
-// document.getElementById("year").innerHTML = date;
+            for (var i = 0; i < spans.length; i++) {
+                if (top > index) {
+                    break;
+                }
 
-// ///
+                var newOffset = spans[i].offsetTop;
 
+                if (newOffset !== offset) {
+                    offset = newOffset;
+                    top++;
+                }
 
-// ///
-// /*
-//  Inspired by https://soulwire.co.uk/
-// */
+                if (top === index) {
+                    buffer.push(spans[i]);
+                }
+            }
 
-// // TODO: revert back to source text after animation is complete
-// // Don't wrap the <span> around the source text, clone first -> then wrap
+            var text = [];
 
-// const hypertext = (function() {
-//     function countLines(elem) {
-//         let lineHeight = window.getComputedStyle(elem, null).getPropertyValue('line-height');
+            for (var i = 0; i < buffer.length; i++) {
+                text.push(buffer[i]);
+            }
 
-//         //     Box Height          Line Height
-//         return elem.offsetHeight / parseInt(lineHeight);
-//     }
+            return text;
+        }
 
-//     function getLines(el) {
-//         let lineCount = countLines(el);
-//         let html = el.innerHTML.replace(/(\S+\s*)/g, '<span>$1</span>');
+        var lines = [];
 
-//         el.innerHTML = html;
+        for (var i = 0; i < lineCount; i++) {
+            lines.push(getLine(i + 1));
+        }
 
-//         var offset = 0;
-//         var spans = document.querySelectorAll('.hypertext span');
+        return lines;
+    }
 
-//         function getLine(index) {
-//             var top = 0,
-//                 buffer = [];
+    function replaceAt(str, index, char) {
+        return str.substr(0, index) + char + str.substr(index + char.length);
+    }
 
-//             for (var i = 0; i < spans.length; i++) {
-//                 if (top > index) {
-//                     break;
-//                 }
+    let probability = function(n) {
+        return !!n && (Math.random() * 10) <= n;
+    }
 
-//                 var newOffset = spans[i].offsetTop;
+    function hypertextChar(char) {
+        return probability(0.65) ? '/' : '-';
+    }
 
-//                 if (newOffset !== offset) {
-//                     offset = newOffset;
-//                     top++;
-//                 }
+    // -------------------------------------------------------------------
 
-//                 if (top === index) {
-//                     buffer.push(spans[i]);
-//                 }
-//             }
+    function editChar(el, text) {
+        let index = 0;
 
-//             var text = [];
-
-//             for (var i = 0; i < buffer.length; i++) {
-//                 text.push(buffer[i]);
-//             }
-
-//             return text;
-//         }
-
-//         var lines = [];
-
-//         for (var i = 0; i < lineCount; i++) {
-//             lines.push(getLine(i + 1));
-//         }
-
-//         return lines;
-//     }
-
-//     function replaceAt(str, index, char) {
-//         return str.substr(0, index) + char + str.substr(index + char.length);
-//     }
-
-//     let probability = function(n) {
-//         return !!n && (Math.random() * 10) <= n;
-//     }
-
-//     function hypertextChar(char) {
-//         return probability(0.65) ? '/' : '-';
-//     }
-
-//     // -------------------------------------------------------------------
-
-//     function editChar(el, text) {
-//         let index = 0;
-
-//         const step = setInterval(() => {
-//             if (text[index] === undefined) return;
+        const step = setInterval(() => {
+            if (text[index] === undefined) return;
             
-//             el.innerText = replaceAt(el.innerText, index, text[index]);
-//             index++;
+            el.innerText = replaceAt(el.innerText, index, text[index]);
+            index++;
 
-//             // If this was the last one, clear the interval
-//             if (index == text.length) clearInterval(step);
-//         }, 50);
-//     }
+            // If this was the last one, clear the interval
+            if (index == text.length) clearInterval(step);
+        }, 50);
+    }
 
-//     function animateChar(el, word) {
-//         let text = word.innerText.replace(/\s/g, '');
-//         let index = 0;
+    function animateChar(el, word) {
+        let text = word.innerText.replace(/\s/g, '');
+        let index = 0;
 
-//         // RESET
-//         el.innerText = '';
+        // RESET
+        el.innerText = '';
 
-//         const step = setInterval(() => {
-//             el.innerText += hypertextChar(text[index]);
-//             index++;
+        const step = setInterval(() => {
+            el.innerText += hypertextChar(text[index]);
+            index++;
 
-//             // If this was the last one, clear the interval
-//             if (index == text.length) {
-//                 clearInterval(step);
-//                 editChar(el, text);
-//             }
-//         }, 50);
-//     }
+            // If this was the last one, clear the interval
+            if (index == text.length) {
+                clearInterval(step);
+                editChar(el, text);
+            }
+        }, 50);
+    }
 
-//     function animateLine(args) {
-//         let { clone, data, index } = args;
+    function animateLine(args) {
+        let { clone, data, index } = args;
 
-//         const row = clone.querySelector('.ht-row:nth-of-type(' + index + ')');
+        const row = clone.querySelector('.ht-row:nth-of-type(' + index + ')');
 
-//         // Start at 0
-//         let i = 0;
+        // Start at 0
+        let i = 0;
 
-//         const step = setInterval(() => {
-//             let span = document.createElement('span');
-//             span.classList.add('ht-word');
+        const step = setInterval(() => {
+            let span = document.createElement('span');
+            span.classList.add('ht-word');
             
-//             row.insertAdjacentHTML('beforeend', String.fromCharCode(160));
-//             row.insertAdjacentElement('beforeend', span);
+            row.insertAdjacentHTML('beforeend', String.fromCharCode(160));
+            row.insertAdjacentElement('beforeend', span);
 
-//             // ---------------------------------------------------------
+            // ---------------------------------------------------------
 
-//             animateChar(span, data[i]);
-//             i++;
+            animateChar(span, data[i]);
+            i++;
 
-//             if (i == data.length) clearInterval(step);
-//         }, 130);
-//     }
+            if (i == data.length) clearInterval(step);
+        }, 130);
+    }
 
-//     function _renderText(el, settings) {
-//         // Create a new element based off the 'el' nodeName (eg. 'p')
-//         const clone = document.createElement(el.nodeName);
-//         clone.classList.add('hypertext-clone');
+    function _renderText(el, settings) {
+        // Create a new element based off the 'el' nodeName (eg. 'p')
+        const clone = document.createElement(el.nodeName);
+        clone.classList.add('hypertext-clone');
 
-//         const lines = getLines(el);
+        const lines = getLines(el);
 
-//         el.style.display = 'none';
-//         el.insertAdjacentElement('afterend', clone);
+        el.style.display = 'none';
+        el.insertAdjacentElement('afterend', clone);
 
-//         // -------------------------------------------------------------------------
+        // -------------------------------------------------------------------------
 
-//         let i = lines.length;
+        let i = lines.length;
 
-//         while (i--) {
-//             let div = document.createElement('div');
-//             div.classList.add('ht-row');
+        while (i--) {
+            let div = document.createElement('div');
+            div.classList.add('ht-row');
 
-//             // TODO: find a better way give .ht-row it's height
-//             // 	     at the moment a &nbsp is being used... eww
-//             div.innerHTML = '&nbsp;';
+            // TODO: find a better way give .ht-row it's height
+            // 	     at the moment a &nbsp is being used... eww
+            div.innerHTML = '&nbsp;';
 
-//             clone.insertAdjacentElement('beforeend', div);
-//         }
+            clone.insertAdjacentElement('beforeend', div);
+        }
 
-//         // -------------------------------------------------------------------------
+        // -------------------------------------------------------------------------
 
-//         let timing = -50;
-//         let index = 0;
+        let timing = -50;
+        let index = 0;
 
-//         let animatedLines = lines.map((data) => {
-//             timing += 200;
+        let animatedLines = lines.map((data) => {
+            timing += 200;
 
-//             return setTimeout(() => {
-//                 index++;
-//                 return animateLine({
-//                     clone,
-//                     data,
-//                     index
-//                 });
-//             }, timing);
-//         });
-//     }
+            return setTimeout(() => {
+                index++;
+                return animateLine({
+                    clone,
+                    data,
+                    index
+                });
+            }, timing);
+        });
+    }
 
-//     // Handler function
-//     function init(config) {
-//         const defaults = {
-//             'selector': '.hypertext'
-//         };
+    // Handler function
+    function init(config) {
+        const defaults = {
+            'selector': '.hypertext'
+        };
 
-//         const settings = Object.assign(defaults, config);
+        const settings = Object.assign(defaults, config);
 
-//         // ----------------------------------------------------
+        // ----------------------------------------------------
 
-//         const elements = document.querySelectorAll(settings.selector);
+        const elements = document.querySelectorAll(settings.selector);
 
-//         return Object.keys(elements).map((key) => {
-//             return _renderText(elements[key]);
-//         });
-//     }
+        return Object.keys(elements).map((key) => {
+            return _renderText(elements[key]);
+        });
+    }
 
-//     return {
-//         init
-//     };
-// })();
+    return {
+        init
+    };
+})();
 
-// hypertext.init({
-//     'selector': '.hypertext'
-// });
+hypertext.init({
+    'selector': '.hypertext'
+});
